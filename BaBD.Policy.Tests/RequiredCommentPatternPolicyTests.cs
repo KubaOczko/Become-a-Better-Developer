072f0bc459ee9be3710e8bb363214c313f21c21f
Kuba Oczko <kuba.oczko@gmail.com>
2015-04-23 22:47:29 +0200

    Create RequiredCommentPatternPolicyTests.cs

---
+++ BaBD.Policy.Tests/RequiredCommentPatternPolicyTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaBD.Policy.Tests
+{
+    [TestClass]
+    public class RequiredCommentPatternPolicyTests
+    {
+        [TestMethod]
+        public void TestCommentMatch()
+        {
+            using (RequiredCommentPatternPolicy policy = new RequiredCommentPatternPolicy())
+            {
+                Assert.IsTrue(policy.CommentMatchesPattern("Task-123 Comment"));
+
+                Assert.IsFalse(policy.CommentMatchesPattern("task123"));
+                Assert.IsFalse(policy.CommentMatchesPattern("task-"));
+                Assert.IsFalse(policy.CommentMatchesPattern("general text"));
+            }
+        }
+    }
+}
